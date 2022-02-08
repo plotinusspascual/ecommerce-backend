7125ba1f21a843751cf084bf8fb7a20e42af1d30
plotinusspascual <plotinusspascual@gmail.com>
2022-02-07 20:30:06 -0800

    associated methods on sequalize models to crete relationships

--- models/index.js
+++ models/index.js
@@ -5,13 +5,29 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+});
 // Categories have many Products
-
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+});
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(Tag, {
+  through: {
+    model: 'product_tag',
+    unique: false,
+  },
+  as: 'product_tags',
+});
 // Tags belongToMany Products (through ProductTag)
-
+Tag.belongsToMany(Product, {
+  through: {
+    model: 'product_tag',
+    unique: false,
+  },
+  as: 'product_tagged'
+})
 module.exports = {
   Product,
   Category,
